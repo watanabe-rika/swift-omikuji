e5207e9b9687c01bb4da80bbdb008e5f020c5458
watanabe rika <watanaberika.13@gmail.com>
2018-04-26 12:28:11 +0900

    完成

--- omikuji/ViewController.swift
+++ omikuji/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+
+    @IBAction func getOmikuji(_ sender: Any) {
+        // おみくじの配列を定義
+        let results = ["かず吉", "りか吉", "じゃこ吉", "ひな吉", "つか吉"]
+
+        // 乱数を生成する
+        let random  = arc4random_uniform(UInt32(results.count))
+        
+        // myLabelに表示する
+        self.myLabel.text = results[Int(random)]
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 正円にする
+        myLabel.layer.cornerRadius  = myLabel.bounds.width / 2
+
+        // 外縁をなくす
+        myLabel.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
